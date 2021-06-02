f3cdca99153d57b040f721d0e565e62517a83ff3
priyanka610 <84711780+priyanka610@users.noreply.github.com>
2021-06-02 18:01:43 +0530

    updated swagger config

--- src/main/java/com/hcl/wm/order/OrderApplication.java
+++ src/main/java/com/hcl/wm/order/OrderApplication.java
@@ -1,10 +1,12 @@
 package com.hcl.wm.order;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@EnableSwagger2
+@OpenAPIDefinition(info = @Info(title = "Customer API", version = "2.0", description = "Customers Information"))
 public class OrderApplication {
 
     public static void main(String[] args) {
